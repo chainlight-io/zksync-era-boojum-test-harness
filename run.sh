9d70c88936abcd38f0896b2874487dfb61e697ca
Andrew Wesie <andrew@chainlight.io>
2023-10-23 03:36:03 +0000

    Increase thread stack size

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd zkevm_test_harness && cargo test --jobs 1 -- --nocapture run_simple
+cd zkevm_test_harness && RUST_MIN_STACK=$((8*1024*1024)) cargo test --jobs 1 -- --nocapture run_simple
